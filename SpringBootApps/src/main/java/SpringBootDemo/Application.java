e11a43de6bf1f7fd77aac0d619ab46aa4b10ac02
Joseph Haddison <joseph@edwiki.in>
2024-10-11 21:39:27 -0400

    New Files Added

--- SpringBootApps/src/main/java/SpringBootDemo/Application.java
+++ SpringBootApps/src/main/java/SpringBootDemo/Application.java
@@ -4,7 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@RestController
 public class Application {
+
+	@GetMapping("/get")
+    public String getMessage(){
+
+        return "Lord Thank You For Blessing My Skills.Your Humble Servant Joseph Haddison";
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
